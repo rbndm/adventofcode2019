a82de62031abb4f01598a36a177ca9f6ee08e069
rbndm <rubendopico@gmail.com>
2019-12-02 00:06:29 +0000

    Update day1_puzzle2.js
    After looking some solutions, some optimizations:
    - We don't need an array in fuel() function. We can rely on recursion stack to add the results
    - No need for map(w => fuel(w)). We can do it in the reduce part

--- day1_puzzle2.js
+++ day1_puzzle2.js
@@ -1,18 +1,17 @@
 /*
 * This is the recursive function needed to calculate the fuel needed, including fuel's weight
-* @param weightArr array containing the module weight for which we want to calculate the fuel needed to fly
+* @param weight module weight for which we want to calculate the fuel needed to fly
 */
-let fuel = (weightArr) => {
+let fuel = (weight) => {
   // Calculates fuel for last array element
-	let _fuel = Math.floor(weightArr[weightArr.length - 1] / 3) - 2;
+	let _fuel = Math.floor(weight / 3) - 2;
   // If fuel needed is > 0, we push it to the array and call fuel() again
 	if(_fuel > 0) {
-		weightArr.push(_fuel);
-		return fuel(weightArr);
+		return _fuel + fuel(_fuel);
 	} else {
     // Otherwise, we reduce the array to sum all the fuel values, except the first element
     // because it is module's weight
-		return weightArr.slice(1).reduce((accu, w) => accu + w, 0);
+		return 0;
 	}
 }
 
@@ -23,10 +22,7 @@ document.body.getElementsByTagName('pre')[0].innerHTML.split('\n').map(
 ).filter(
   // Remove NaN's
   w=>!isNaN(w)
-).map(
-  // Map each weight w in the array to fuel([w])
-  w=>fuel([w])
 ).reduce(
   // Reduce the array to the sum of all fuel results
-  (acc, weight) => acc + weight, 0
+  (acc, weight) => acc + fuel(weight), 0
 );
